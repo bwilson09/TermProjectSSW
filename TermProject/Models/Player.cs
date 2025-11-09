c7a25267a821449534d29c85905dd0a191889bb5
bwilson09 <bwilson09@mynbcc.ca>
2025-11-09 19:11:46 -0400

    Added EF Packages and created models for team and player

---
+++ TermProject/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace TermProject.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public int TeamId { get; set; }
+
+        public Player() { }
+    }
+}
