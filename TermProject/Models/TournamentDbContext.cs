a18ae2a2496166929456830509381e5c89dbfc9a
bwilson09 <bwilson09@mynbcc.ca>
2025-11-17 09:07:24 -0400

    added the Db Context class

---
+++ TermProject/Models/TournamentDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TermProject.Models
+{
+    public class TournamentDbContext: DbContext
+    {
+        public TournamentDbContext(DbContextOptions<TournamentDbContext> options)
+            : base(options) { }
+
+
+        //confirm databse table names in db
+        public DbSet<Team> Team => Set<Team>();
+
+        public DbSet<Player> Player => Set<Player>();
+
+        public DbSet<User> User => Set<User>();
+
+        //force singular table name?? - again double check db names
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Team>().ToTable("Team");
+            modelBuilder.Entity<Player>().ToTable("Player");
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
