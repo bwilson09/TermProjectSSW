26970b91b288f404d1919b5934f8c43d2f961d5e
bwilson09 <bwilson09@mynbcc.ca>
2025-11-17 11:36:35 -0400

    added controller to load Register page

--- TermProject/Controllers/TournamentController.cs
+++ TermProject/Controllers/TournamentController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TermProject.Models;
 
 namespace TermProject.Controllers
 {
     public class TournamentController : Controller
     {
+
+        private readonly TournamentDbContext _db;
+
+        //constructor receives the inject DbContext
+        public TournamentController(TournamentDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
         {
             return View();
         }
