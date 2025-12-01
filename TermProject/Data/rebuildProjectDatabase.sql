efd36c15091d56ea60a577dda436bd94801327ea
bwilson09 <bwilson09@mynbcc.ca>
2025-12-01 10:56:35 -0400

    added admin user

--- TermProject/Data/rebuildProjectDatabase.sql
+++ TermProject/Data/rebuildProjectDatabase.sql
@@ -42,7 +42,8 @@ CREATE TABLE Player (
 );
 
 -- BowlingUser records
-INSERT into BowlingUser values (1, 'smonk', 'AQAAAAIAAYagAAAAEIqCPZxk8RTs3R6K/dYZAWlDgCr83d3U3MDums/YuZEQl/gXqc+e7AsNVHp3jMBtOg==', 1);
+INSERT into BowlingUser values (1, 'smonk', 'AQAAAAIAAYagAAAAEIqCPZxk8RTs3R6K/dYZAWlDgCr83d3U3MDums/YuZEQl/gXqc+e7AsNVHp3jMBtOg==', 1),
+('user', 'AQAAAAEAACcQAAAAEKnxsaPZ8lp+KXyoiMdENH4M3VotsCRT7LU1Xr1YlnDoaRXpkROBT7l9OGKhXiEh7Q==', 1);
 
 -- Division records
 INSERT INTO Division VALUES (1, 'Men''s');
